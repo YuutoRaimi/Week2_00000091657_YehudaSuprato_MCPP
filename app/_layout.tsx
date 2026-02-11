81b4fce932174cd2019e247599a95e6949f1c84e
YuutoRaimi <playzyehuda@gmail.com>
2026-02-11 15:55:15 +0700

    Profile App

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,16 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerTitle: "Profile App",
+          headerStyle: { backgroundColor: "#000000" },
+          headerTitleStyle: { color: "#FFFFFF" },
+        }}
+      />
+    </Stack>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,66 @@
-import { Text, View } from "react-native";
+import React from "react";
+import { Image, ScrollView, StyleSheet, Text } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <Image
+        source={require("../assets/images/foto.png")}
+        style={styles.image}
+      />
+      <Text style={styles.name}>Yehuda Suprato</Text>
+      <Text style={styles.nim}>00000091657</Text>
+      <Image
+        source={require("../assets/images/Cute.jpeg")}
+        style={styles.image}
+      />
+      <Text style={styles.name}>Reine Kyoko</Text>
+      <Text style={styles.nim}>I Like Purple Color</Text>
+      <Image
+        source={require("../assets/images/black white.jpeg")}
+        style={styles.image}
+      />
+      <Text style={styles.name}>Ms. Shiro Kyoko</Text>
+      <Text style={styles.nim}>My Favorite Food is Ramen</Text>
+      <Image
+        source={require("../assets/images/miko.jpg")}
+        style={styles.image}
+      />
+      <Text style={styles.name}>Sakura Miko</Text>
+      <Text style={styles.nim}>Hololive Gen 0</Text>
+      <Image
+        source={require("../assets/images/Gigi.jpg")}
+        style={styles.image}
+      />
+      <Text style={styles.name}>Gigi Murin</Text>
+      <Text style={styles.nim}>Hololive Advent</Text>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000000",
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 100,
+    alignSelf: "center",
+    marginTop: 50,
+  },
+  name: {
+    fontSize: 20,
+    textAlign: "center",
+    marginTop: 20,
+    fontWeight: "bold",
+    color: "#FFFFFF",
+  },
+  nim: {
+    fontSize: 16,
+    textAlign: "center",
+    marginTop: 10,
+    color: "#ff00d4",
+  },
+});
